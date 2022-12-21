aeb438049b7429205bd94a57e83c46cb7d1380d3
Jerome <jerome56jj@hotmail.com>
2022-12-21 11:00:36 +0100

    create projects

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="project">
+    <div class="project" :class="{ complete: project.complete}">
         <div class="actions">
             <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
             <div class="icons">
                 <span class="material-icons"> edit </span>
                 <span @click="deleteProject" class="material-icons"> delete </span>
-                <span @click="toggleComplete" class="material-icons"> done </span>
+                <span @click="toggleComplete" class="material-icons tick"> done </span>
             </div>
         </div>
         <div v-if="showDetails" class="details">
@@ -71,5 +71,12 @@ h3 {
 
 .material-icons:hover {
     color: #777
+}
+/* completed projects */
+.project.complete{
+    border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+    color: #00ce89;
 }
 </style>
