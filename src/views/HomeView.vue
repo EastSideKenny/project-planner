acba17263624798b7d120d27a559825585114440
Jerome <jerome56jj@hotmail.com>
2022-12-21 10:17:51 +0100

    handle delete and complete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project"/>
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
   </div>
@@ -13,17 +13,30 @@ import SingleProject from '../components/SingleProject.vue'
 
 export default {
   name: 'HomeView',
-  components: {SingleProject},
-  data(){
-    return{
+  components: { SingleProject },
+  data() {
+    return {
       projects: []
     }
   },
-  mounted(){
+  mounted() {
     fetch('http://localhost:3000/projects')
       .then(res => res.json())
       .then(data => this.projects = data)
       .catch(err => console.log(err.message))
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id
+      })
+    },
+    handleComplete(id){
+      let proj = this.projects.find(project => {
+        return project.id === id
+      })
+      proj.complete = !proj.complete
+    }
   }
 }
 </script>
